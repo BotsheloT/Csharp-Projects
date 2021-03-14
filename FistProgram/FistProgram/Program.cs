f0d3cd6444c0f7e26dbc45d919c299c19e1e3c92
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2021-03-14 20:34:15 +0200

    Add files via upload

---
+++ FistProgram/FistProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FistProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            String name = Console.ReadLine();     
+            Console.WriteLine("Hello " + name + " welcome to C#");            
+        }
+    }
+}
