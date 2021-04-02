91254d1d429c419565e9886440059d891fe85b7a
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2021-04-02 23:41:29 +0200

    Add files via upload
    This was my first attempt at using foreach statements for displaying array items instead of a traditional for loop.

---
+++ Foreach Statements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration of Array with names
+            string[] names = {"Botshelo", "Masego", "Boitumelo", "Tshepiso"};
+            //display with foreach statement
+            Console.WriteLine("The Tlhabanyane Family Consists of\t>>");
+            foreach (string stuff in names)
+            {
+                Console.WriteLine(stuff);
+            }
+        }
+    }
+}
